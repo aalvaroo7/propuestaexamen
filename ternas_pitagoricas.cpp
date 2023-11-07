c7a2355fa8bc8e381c0409c2776ff30fbef86166
aalvaroo7 <almarrom@myuax.com>
2023-11-07 12:11:01 +0100

    comit main

--- ternas_pitagoricas.cpp
+++ ternas_pitagoricas.cpp
@@ -3,12 +3,10 @@
 #include <cmath>
 using namespace std;
 int main() {
-    int n = 4; //numero de ternas pitagoricas que deseamos calcular(las 4 primeras)
     int i;
-    for (i=1;i<5;i++);//contador de ternas
     cout<< "que terna pitagorica deseas calcular"<<endl;
     cin>>i;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1;i<5; i++) {
         for (int a = 1; a <= 20; a++) {
             for (int b = a + 1; b <= 20; b++) {
                 int c = sqrt(a * a + b * b);
