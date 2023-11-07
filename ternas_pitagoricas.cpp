8a11b1c694a2bbfd69c142e22a836d9c18499ec4
aalvaroo7 <almarrom@myuax.com>
2023-11-07 11:50:49 +0100

    comit main

--- ternas_pitagoricas.cpp
+++ ternas_pitagoricas.cpp
@@ -2,23 +2,6 @@
 #include <cmath>
 using namespace std;
 
-int main(){
-    int a;
-    cout<<"ingrese el primer lado"<<endl;
-    cin>>a;
-for(a=0;a<10;a++) {
-    if (a == 3 || a == 5 || a == 6 || a == 8)
-        continue;
-    int b;
-    cout << "ingrese el segundo lado" << endl;
-    cin >> b;
-    for (b = 0; b < 20; b++) {
-        while (b==4 ||b==8||b== 15)
-            continue;
-        int c;
-        c = sqrt(a * a + b * b);
-        cout << "el triangulo tiene lados" << a << "," << b << "," << c << endl;
-    }
-}
-return 0;
-}
+
+int main() {
+    int n;
