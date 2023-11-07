4f801af921aea8b4827287eacf8db089dfc50f47
aalvaroo7 <almarrom@myuax.com>
2023-11-07 12:01:50 +0100

    comit main

--- ternas_pitagoricas.cpp
+++ ternas_pitagoricas.cpp
@@ -6,4 +6,10 @@ int main(){
     for (int i = 1; i <= n; i++) {
         for (int a = 1; a <= 20; a++) {
             for (int b = a + 1; b <= 20; b++) {
+                int c = sqrt(a * a + b * b);
+                if (c * c == a * a + b * b) {
+                    cout << "Terna Pitagórica #" << i << ": " << a << ", " << b << ", " << c << endl;
+                    i++;
+                }
+            }
 }
