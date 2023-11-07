ad8de29e4c9fee4374ffbdd4a359a15a44e588a5
aalvaroo7 <almarrom@myuax.com>
2023-11-07 11:53:05 +0100

    comit main

--- ternas_pitagoricas.cpp
+++ ternas_pitagoricas.cpp
@@ -2,6 +2,9 @@
 #include <cmath>
 using namespace std;
 
+// Programa que imprime todas las ternas pitagóricas
+// para las cuales a, b y c son menores o iguales a n.
+
 
 int main() {
     int n;
@@ -9,3 +12,9 @@ int main() {
     cin >> n;
     for (int c = 1; c <= n; c++) {
         for (int b = 1; b < c; b++) {
+            for (int a = 1; a < b; a++) {
+                if (a*a + b*b == c*c) {
+                    cout << a << " " << b << " " << c << endl;
+                }
+            }
+        }
