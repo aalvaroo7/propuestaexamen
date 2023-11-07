4476b4f9a3cb511ec7ca73b5b787bfda27136e6a
aalvaroo7 <almarrom@myuax.com>
2023-11-07 12:01:32 +0100

    comit main

--- ternas_pitagoricas.cpp
+++ ternas_pitagoricas.cpp
@@ -1,24 +1,9 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
-#include <iostream>
-#include <cmath>
-using namespace std;
-
 int main(){
-    int a;
-    for(a=0;a<10;a++){
-        cout<<"ingrese el primer lado"<<endl;
-        cin>>a;
-        for(int b=0;b<20;b++)
-            cout<<"ingrese el segundo lado"<<endl;
-        cin>>b;
-        while(a*a+b*b!=c*c)
-            continue;
-
-
-    }
-    cout<<a<<b<<c<<endl;
-    return 0;
+    int n=4 //numero de ternas pitagoricas que deseamos calcular(las 4 primeras)
+    for (int i = 1; i <= n; i++) {
+        for (int a = 1; a <= 20; a++) {
+            for (int b = a + 1; b <= 20; b++) {
 }
