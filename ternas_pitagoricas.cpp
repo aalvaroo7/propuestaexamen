df8bc28108b59e51e41d1773f0ef175208b2c11c
aalvaroo7 <almarrom@myuax.com>
2023-11-07 11:17:19 +0100

    comit main

--- ternas_pitagoricas.cpp
+++ ternas_pitagoricas.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 int main(){
     int a;
-for(a=0;a<10;a++)
+for(a=0;a<10;a++){
+ for(int b=0;b<20;b++);
+cout<<"ingrese su numero"<<endl;
+cin<<a<<endl;
 
 while(a*a+b*b!=c*c)
 continue;
