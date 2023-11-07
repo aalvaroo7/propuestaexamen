043f39c608fbf130b98725c1815eeee434415799
aalvaroo7 <almarrom@myuax.com>
2023-11-07 11:46:37 +0100

    comit main

--- ternas_pitagoricas.cpp
+++ ternas_pitagoricas.cpp
@@ -13,7 +13,7 @@ for(a=0;a<10;a++) {
     cout << "ingrese el segundo lado" << endl;
     cin >> b;
     for (b = 0; b < 20; b++) {
-        while (b==4 8, 12, 15)
+        while (b==4 ||b==8||b== 15)
             continue;
         int c;
         c = sqrt(a * a + b * b);
