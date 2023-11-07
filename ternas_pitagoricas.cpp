e38a8bb71f310d285057f665d74fdbbfecc6f1f3
aalvaroo7 <almarrom@myuax.com>
2023-11-07 11:51:44 +0100

    comit main

--- ternas_pitagoricas.cpp
+++ ternas_pitagoricas.cpp
@@ -5,3 +5,7 @@ using namespace std;
 
 int main() {
     int n;
+    cout << "Ingrese el valor máximo para c: ";
+    cin >> n;
+    for (int c = 1; c <= n; c++) {
+        for (int b = 1; b < c; b++) {
