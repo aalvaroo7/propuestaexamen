21264e1c01f7dee3446634d78e87ed7e3f360de4
aalvaroo7 <almarrom@myuax.com>
2023-11-07 12:17:45 +0100

    comit main

--- ternas_pitagoricas.cpp
+++ ternas_pitagoricas.cpp
@@ -3,12 +3,15 @@
 #include <cmath>
 using namespace std;
 int main() {
-    int a
+    int a;
         cout<<"ingrese el primer lado"<<endl;
         cin>>a;
         for (int a = 3; a <= 20; a++) {
-            while(a!=3||a!=5||a!=6||a!=8)
-            for (int b = a + 1; b <= 20; b++) {
+            while(a==3||a=5||a==6||a==8)
+                if else break;
+            for (int b=4; b <= 20; b++) {
+                while(b==4||b==12||b==15||b==8)
+                    if else break;
                 int c = sqrt(a * a + b * b);
                 if (c * c == a * a + b * b) {
                     cout << "Terna Pitagórica #" << i << ": " << a << ", " << b << ", " << c << endl;
